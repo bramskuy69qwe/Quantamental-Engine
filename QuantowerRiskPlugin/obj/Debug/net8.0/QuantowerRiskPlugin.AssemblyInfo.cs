7421df0b4ab18ce9397de77b30ff8aa18d57e842
bramskuy69qwe <antonyarde@gmail.com>
2026-05-04 19:18:34 +0700

    risk plugin addition

--- QuantowerRiskPlugin/obj/Debug/net8.0/QuantowerRiskPlugin.AssemblyInfo.cs
+++ QuantowerRiskPlugin/obj/Debug/net8.0/QuantowerRiskPlugin.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantowerRiskPlugin")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Risk Engine integration plugin for Quantower")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a724ffda8f88b047cf528ad69a69ea209db39875")]
