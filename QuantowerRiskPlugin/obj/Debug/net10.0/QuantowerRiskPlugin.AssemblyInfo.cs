263983d4de56a65f51e80f56f88a5bd18054c881
bramskuy69qwe <antonyarde@gmail.com>
2026-05-13 20:27:48 +0700

    misc

--- QuantowerRiskPlugin/obj/Debug/net10.0/QuantowerRiskPlugin.AssemblyInfo.cs
+++ QuantowerRiskPlugin/obj/Debug/net10.0/QuantowerRiskPlugin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Risk Engine integration plugin for Quantower")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8f2860e1453c4bec77b27c98f4edeb10be03ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405efbbf897bf7ae1ba13d59f522a87b618f82dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantowerRiskPlugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantowerRiskPlugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
